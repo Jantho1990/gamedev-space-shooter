d9a904e5f14f1637bf2d2a8a1fb9f4962bb9cd7f
Joshua Anthony <jantho1990@gmail.com>
2018-05-08 22:14:54 -0500

    got spaceships to render

--- src/main.js
+++ src/main.js
@@ -1,62 +1,55 @@
-import KeyControls from "../lib/KeyControls"
-import MouseControls from "../lib/MouseControls"
-
-const canvas = document.querySelector('#board canvas')
-const ctx = canvas.getContext("2d")
-console.log(ctx.canvas)
-
-ctx.strokeStyle = "#FFF"
-
-const {
-    width: w,
-    height: h
-} = canvas
+import pop from "../pop/index"
+const { CanvasRenderer, Container, Sprite, Text, Texture } = pop
 
 // Game setup
+const w = 640
+const h = 480
+const renderer = new CanvasRenderer(w, h)
+document.querySelector("#board").appendChild(renderer.view)
+
+const scene = new Container()
+
+const texture = new Texture('res/img/spaceship.png')
+
+for (let i = 0; i < 50; i++) {
+    const ship = new Sprite(texture)
+    ship.pos.x = Math.random() * w
+    ship.pos.y = Math.random() * h
+    scene.add(ship)
+}
+
+const message = new Text("The Renderer!", {
+    font: "40pt sans-serif",
+    fill: "#AF111C",
+    align: "center"
+})
+message.pos.x =  w / 2
+console.log(typeof message.pos.x, 'hh')
+message.pos.y = h / 2
+message.update = function (dt) {
+    // console.log(typeof this.pos.x, typeof (100 * dt), typeof dt, 'sh')
+    this.pos.x = Math.round(this.pos.x -  (100 * dt), 0)
+    // console.log(this.pos.x - (100 * dt))
+    // console.log(this.pos.x)
+    if (this.pos.x < -420) {
+        this.pos.x = w
+    }
+}
+scene.add(message)
+
+console.log(scene.children)
+
 let dt = 0
 let last = 0
 
-let x = w / 2
-let y = h / 2
-let color = 0
-
-let bw = 50
-let bh = 50
-
-const controls = new KeyControls()
-const mouse = new MouseControls(canvas)
-
 function loop (ms) {
     requestAnimationFrame(loop)
-    
-    const t = ms / 1000 // convert to seconds
+
+    const t = ms / 1000
     dt = t - last
     last = t
-    
-    
-    // Game logic
-    /* ctx.fillStyle = "#000"
-    ctx.fillRect(0, 0, w, h)
-    ctx.strokeText(`Frame length: ${(dt * 1000).toFixed(2)} ms`, 70, 50)
-    ctx.strokeText(`Total time: ${t.toFixed(2)}`, 70, 90) */
 
-    x = mouse.pos.x
-    y = mouse.pos.y
-    if (controls.action) {
-        color += 10
-        if (color > 360) {
-            color -= 360
-        }
-    }
-
-    bw >= 1 ? bw += controls.x : bw = 1
-    bh >= 1 ? bh += -(controls.y) : bh = 1
-
-    // draw the rectangle
-    if (mouse.isDown) {
-        ctx.fillStyle = `hsl(${color}, 50%, 50%)`
-        ctx.fillRect(x, y, bw, bh)
-    }
-    mouse.update()
+    scene.update(dt, t)
+    renderer.render(scene)
 }
 requestAnimationFrame(loop)
